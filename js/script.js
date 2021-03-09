0c30a05fb647777a23e637c8c21e108b63083245
Daniel-1703 <d.daniil17.mart@gmail.com>
2021-03-10 01:14:28 +0300

    refactoring code and fixing a bug in the form

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const tabs = require('./modules/tabs'),
-          modal = require('./modules/modal'),
-          timer = require('./modules/timer'),
-          slider = require('./modules/slider'),
-          cards = require('./modules/cards'),
-          calc = require('./modules/calc'),
-          forms = require('./modules/forms');
+import tabs from './modules/tabs';
+import modal from './modules/modal';
+import timer from './modules/timer';
+import slider from './modules/slider';
+import cards from './modules/cards';
+import calc from './modules/calc';
+import forms from './modules/forms';
+import {openModal} from './modules/modal';
 
-    tabs();
-    modal();
-    timer();
-    slider();
+window.addEventListener('DOMContentLoaded', () => {
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 15000);
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2021-05-04');
+    slider({
+        container: '.offer__slider',
+        slide: '.offer__slide',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
     cards();
     calc();
-    forms();
+    forms('form', modalTimerId);
 });
